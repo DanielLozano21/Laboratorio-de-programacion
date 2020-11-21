d0cb27c8b3f26b3b917e092bd91128e3ae33b109
Daniel <dany_z00@hotmail.com>
2020-11-21 11:24:28 -0600

    Commit de la Practica 9

---
+++ Practica9.sh
@@ -0,0 +1,55 @@
+#Daniel Adrian Lozano Garza
+#!/bin/bash
+
+#Al script le proporcionas 2 parametros uno es la ruta donde tienes el archivo con los correos
+#el siguiente es el mensaje que quieras enviar
+
+emails=$1
+mensaje=$2
+
+function py { python3 -c "$1" "$2" ; }
+
+for email in $(cat $emails)
+do
+    py "
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import json
+    
+def sendmail(a, x):
+
+
+    data = {}
+    with open('pass.json') as f:
+         data = json.load(f)
+    msg = MIMEMultipart()
+
+    message = str(x)
+
+    msg['From'] = data['user']
+
+    msg['To'] = str(a)
+    msg['Subject'] = 'Correo mandado por python'
+
+    msg.attach(MIMEText(message, 'plain'))
+
+    server = smtplib.SMTP('smtp.office365.com:587')
+    server.starttls()
+
+    print(data['user'])
+    server.login(data['user'], data['pass'])
+
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    server.quit()
+
+    print('successfully sent email to %s:' % (msg['To']))
+
+a = '$email'
+b = '$mensaje'
+
+sendmail(a, b)
+    "
+
+done
